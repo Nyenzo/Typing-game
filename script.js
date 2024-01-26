8e229212573733119aa10fceead6af9cb156dbd1
Nyenzo <nyenzoisabwa@gmail.com>
2024-01-26 16:27:18 +0300

    UI updates

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//Time limit
-let TIME_LIMIT = 60; 
-let quotes_array = [
+const quotes = [
     'When you have eliminated the impossible, whatever remains, however improbable, must be the truth.',
     'There is nothing more deceptive than an obvious fact.',
     'I ought to know by this time that when a fact appears to be opposed to a long train of deductions it invariably proves to be capable of bearing some other interpretation.',
@@ -32,174 +30,78 @@ let quotes_array = [
     'Education never ends, Watson. It is a series of lessons, with the greatest for the last.',
 ];
 
-let timer_text = document.querySelector(".curr_time"); 
-let accuracy_text = document.querySelector(".curr_accuracy"); 
-let error_text = document.querySelector(".curr_errors"); 
-let cpm_text = document.querySelector(".curr_cpm"); 
-let wpm_text = document.querySelector(".curr_wpm"); 
-let quote_text = document.querySelector(".quote"); 
-let input_area = document.querySelector(".input_area"); 
-let restart_btn = document.querySelector(".restart_btn"); 
-let cpm_group = document.querySelector(".cpm"); 
-let wpm_group = document.querySelector(".wpm"); 
-let error_group = document.querySelector(".errors"); 
-let accuracy_group = document.querySelector(".accuracy"); 
-  
-let timeLeft = TIME_LIMIT; 
-let timeElapsed = 0; 
-let total_errors = 0; 
-let errors = 0; 
-let accuracy = 0; 
-let characterTyped = 0; 
-let current_quote = ""; 
-let quoteNo = 0; 
-let timer = null; 
+// array for storing the words of the current challenge
+let words = [];
+// stores the index of the word the player is currently typing
+let wordIndex = 0;
+// default value for startTime (will be set on start)
+let startTime = Date.now();
 
-function updateQuote() { 
-    quote_text.textContent = null; 
-    current_quote = quotes_array[quoteNo]; 
-    
-    // separate each character and make an element 
-    // out of each of them to individually style them 
-    current_quote.split('').forEach(char => { 
-        const charSpan = document.createElement('span') 
-        charSpan.innerText = char 
-        quote_text.appendChild(charSpan) 
-    }) 
-    
-    // roll over to the first quote 
-    if (quoteNo < quotes_array.length - 1) 
-        quoteNo++; 
-    else
-        quoteNo = 0; 
-    } 
-function processCurrentText() { 
+// grab UI items
+const quoteElement = document.getElementById('quote');
+const messageElement = document.getElementById('message')
+const typedValueElement = document.getElementById('typed-value');
 
-// get current input text and split it 
-curr_input = input_area.value; 
-curr_input_array = curr_input.split(''); 
+document.getElementById('start').addEventListener('click', function () {
+	// get a quote
+	const quoteIndex = Math.floor(Math.random() * quotes.length);
+	const quote = quotes[quoteIndex];
+	// Put the quote into an array of words
+	words = quote.split(' ');
+	// reset the word index for tracking
+	wordIndex = 0;
 
-// increment total characters typed 
-characterTyped++; 
+	// UI updates
+	// Create an array of span elements so we can set a class
+	const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+	// Convert into string and set as innerHTML on quote display
+	quoteElement.innerHTML = spanWords.join('');
+	// Highlight the first word
+	quoteElement.childNodes[0].className = 'highlight';
+	// Clear any prior messages
+	messageElement.innerText = '';
 
-errors = 0; 
+	// Setup the textbox
+	// Clear the textbox
+	typedValueElement.value = '';
+	// set focus
+	typedValueElement.focus();
+	// set the event handler
 
-quoteSpanArray = quote_text.querySelectorAll('span'); 
-quoteSpanArray.forEach((char, index) => { 
-	let typedChar = curr_input_array[index] 
+	// Start the timer
+	startTime = new Date().getTime();
+});
 
-	// character not currently typed 
-	if (typedChar == null) { 
-	char.classList.remove('correct_char'); 
-	char.classList.remove('incorrect_char'); 
+typedValueElement.addEventListener('input', (e) => {
+	// Get the current word
+	const currentWord = words[wordIndex];
+	// get the current value
+	const typedValue = typedValueElement.value;
 
-	// correct character 
-	} else if (typedChar === char.innerText) { 
-	char.classList.add('correct_char'); 
-	char.classList.remove('incorrect_char'); 
-
-	// incorrect character 
-	} else { 
-	char.classList.add('incorrect_char'); 
-	char.classList.remove('correct_char'); 
-
-	// increment number of errors 
-	errors++; 
-	} 
-}); 
-
-// display the number of errors 
-error_text.textContent = total_errors + errors; 
-
-// update accuracy text 
-let correctCharacters = (characterTyped - (total_errors + errors)); 
-let accuracyVal = ((correctCharacters / characterTyped) * 100); 
-accuracy_text.textContent = Math.round(accuracyVal); 
-
-// if current text is completely typed 
-// irrespective of errors 
-if (curr_input.length == current_quote.length) { 
-	updateQuote(); 
-
-	// update total errors 
-	total_errors += errors; 
-
-	// clear the input area 
-	input_area.value = ""; 
-} 
-} 
-
-function startGame() { 
-
-    resetValues(); 
-    updateQuote(); 
-    
-    // clear old and start a new timer 
-    clearInterval(timer); 
-    timer = setInterval(updateTimer, 1000); 
-} 
-    
-function resetValues() { 
-    timeLeft = TIME_LIMIT; 
-    timeElapsed = 0; 
-    errors = 0; 
-    total_errors = 0; 
-    accuracy = 0; 
-    characterTyped = 0; 
-    quoteNo = 0; 
-    input_area.disabled = false; 
-    
-    input_area.value = ""; 
-    quote_text.textContent = 'Click on the area below to start the game.'; 
-    accuracy_text.textContent = 100; 
-    timer_text.textContent = timeLeft + 's'; 
-    error_text.textContent = 0; 
-    restart_btn.style.display = "none"; 
-    cpm_group.style.display = "none"; 
-    wpm_group.style.display = "none"; 
-} 
-function updateTimer() { 
-    if (timeLeft > 0) { 
-        // decrease the current time left 
-        timeLeft--; 
-    
-        // increase the time elapsed 
-        timeElapsed++; 
-    
-        // update the timer text 
-        timer_text.textContent = timeLeft + "s"; 
-    } 
-    else { 
-        // finish the game 
-        finishGame(); 
-    } 
-} 
-
-function finishGame() { 
-    // stop the timer 
-    clearInterval(timer); 
-    
-    // disable the input area 
-    input_area.disabled = true; 
-    
-    // show finishing text 
-    quote_text.textContent = "Click on restart to start a new game."; 
-    
-    // display restart button 
-    restart_btn.style.display = "block"; 
-    
-    // calculate cpm and wpm 
-    cpm = Math.round(((characterTyped / timeElapsed) * 60)); 
-    wpm = Math.round((((characterTyped / 5) / timeElapsed) * 60)); 
-    
-    // update cpm and wpm text 
-    cpm_text.textContent = cpm; 
-    wpm_text.textContent = wpm; 
-    
-    // display the cpm and wpm 
-    cpm_group.style.display = "block"; 
-    wpm_group.style.display = "block"; 
-} 
-    
-    
-    
+	if (typedValue === currentWord && wordIndex === words.length - 1) {
+		// end of quote
+		// Display success
+		const elapsedTime = new Date().getTime() - startTime;
+		const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
+		messageElement.innerText = message;
+	} else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+		// end of word
+		// clear the typedValueElement for the new word
+		typedValueElement.value = '';
+		// move to the next word
+		wordIndex++;
+		// reset the class name for all elements in quote
+		for (const wordElement of quoteElement.childNodes) {
+			wordElement.className = '';
+		}
+		// highlight the new word
+		quoteElement.childNodes[wordIndex].className = 'highlight';
+	} else if (currentWord.startsWith(typedValue)) {
+		// currently correct
+		// highlight the next word
+		typedValueElement.className = '';
+	} else {
+		// error state
+		typedValueElement.className = 'error';
+	}
+});
